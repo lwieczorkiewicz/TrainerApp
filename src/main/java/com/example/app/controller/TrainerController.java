3336e26b6bac5f0d50914ff084cf2d99f66da0e1
Łukasz Wieczorkiewicz <lwieczorkiewicz@gmail.com>
2021-03-04 15:46:12 +0100

    Added thymeleaf to html files

--- src/main/java/com/example/app/controller/TrainerController.java
+++ src/main/java/com/example/app/controller/TrainerController.java
@@ -4,14 +4,15 @@ import com.example.app.dto.TrainersDto;
 import com.example.app.service.TrainersService;
 import com.example.app.users.Trainers;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
-@RequestMapping("/trainers")
 public class TrainerController {
 
     private final TrainersService trainersService;
@@ -20,6 +21,30 @@ public class TrainerController {
     public TrainersDto saveTrainers(@RequestBody TrainersDto trainersDto) {
 
         return trainersService.saveTrainers(trainersDto);
+    }
 
+    @GetMapping("/trainer")
+    public String main(){
+        return "index";
+    }
+
+    @GetMapping("/addTrainer")
+    public String main1(){
+        return "addTrainer";
+    }
+
+    @GetMapping("/allTrainers")
+    public String main2(){
+        return "allTrainers";
+    }
+
+    @GetMapping("/contact")
+    public String main3(){
+        return "contact";
+    }
+
+    @GetMapping("/updateTrainer")
+    public String main4(){
+        return "updateTrainer";
     }
 }
