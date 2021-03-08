98f536ab03dead02a92007881d233d387e2b8a2b
Łukasz Wieczorkiewicz <lwieczorkiewicz@gmail.com>
2021-03-08 10:26:51 +0100

    Addad Validation to addTrainer form

--- src/main/java/com/example/app/controller/TrainerController.java
+++ src/main/java/com/example/app/controller/TrainerController.java
@@ -6,11 +6,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @Controller
@@ -44,9 +46,12 @@ public class TrainerController {
     }
 
     @PostMapping("/saveTrainer")
-    public String saveTrainer(@ModelAttribute("trainers") Trainers trainers) {
-        trainersService.saveTrainer(trainers);
-        return "redirect:/allTrainers";
+    public String saveTrainer(@Valid @ModelAttribute("trainers") Trainers trainers, BindingResult result) {
+        if (result.hasErrors()) {
+            return "addTrainer";
+        } else
+            trainersService.saveTrainer(trainers);
+            return "redirect:/allTrainers";
     }
 
     @GetMapping("/updateForm/{id}")
--- src/main/java/com/example/app/model/Trainers.java
+++ src/main/java/com/example/app/model/Trainers.java
@@ -8,6 +8,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 @Data
@@ -20,17 +25,23 @@ public class Trainers {
     @Type(type = "org.hibernate.type.UUIDCharType")
     private UUID id;
 
+    @NotBlank(message = "Put your NAME please")
+    @Size(min=2, max = 20, message = "Has to be between 2 and 20 characters")
     @Column(name = "name", nullable = false)
     private String firstName;
 
+    @NotBlank(message = "Put your SURNAME please")
+    @Size(min=2, max = 20, message = "Has to be between 2 and 20 characters")
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
+    @NotBlank(message = "Put your EMAIL please and need to be with '@'")
+    @Email(message = "Wrong EMAIL. Use '@'")
     @Column(name = "email", nullable = false)
     private String email;
 
-    @Column(name = "phone_number", nullable = false)
-    private int phoneNumber;
+    @NotNull(message = "Put your phone please")
+    private Integer phoneNumber;
 
     public Trainers() {
     }
@@ -40,5 +51,15 @@ public class Trainers {
         this.lastName = lastName;
         this.email = email;
         this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Trainers{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
     }
 }
