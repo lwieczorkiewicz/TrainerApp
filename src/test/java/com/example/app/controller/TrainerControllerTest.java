33d641c99a8f8d6150c3bd103b5110f795edaf2a
Łukasz Wieczorkiewicz <lwieczorkiewicz@gmail.com>
2021-03-08 15:52:02 +0100

    Added Validation complite

---
+++ src/test/java/com/example/app/controller/TrainerControllerTest.java
@@ -0,0 +1,11 @@
+package com.example.app.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TrainerControllerTest {
+
+    @Test
+    void mainPage() {
+    }
+}
