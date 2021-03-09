42c9354c9c05358abcf50143956da07419128959
Łukasz Wieczorkiewicz <lwieczorkiewicz@gmail.com>
2021-03-09 08:50:13 +0100

    Added some changes to README.md file

--- README.md
+++ README.md
@@ -2,7 +2,24 @@
 
 ---
 
-Aplikacja wymaga bazy danych, którą można utworzyć na dockerze:
+Aplikacja wymaga bazy danych, ktora mozna utworzyc na dockerze:
 
  docker run -p 0.0.0.0:3306:3307 --name trainers -e MYSQL_ROOT_PASSWORD=toor -e MYSQL_DATABASE=trainers -d mysql
  
+Widoki dzialajacej aplikacji:
+
+
+![Lista_trenerow](/home/lukas/Desktop/Zadanie1/TrainerApp/src/main/resources/static/lista_trenerow.png "Lista Trenerow")
+
+
+![Formularz_dodawania](/home/lukas/Desktop/Zadanie1/TrainerApp/src/main/resources/static/formularz_dodawania.png "Formularz dodawania trenera")
+
+
+
+
+
+
+ 
+## Author
+
+Łukasz Wieczorkiewicz
--- src/test/java/com/example/app/controller/TrainerControllerTest.java
+++ src/test/java/com/example/app/controller/TrainerControllerTest.java
@@ -1,11 +1,60 @@
 package com.example.app.controller;
 
+import com.example.app.model.Trainers;
+import com.example.app.repositories.TrainersRepositories;
+import com.example.app.service.TrainersService;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
 class TrainerControllerTest {
+
+    @Mock
+    TrainersRepositories trainersRepositories;
+
+    @Mock
+    TrainersService trainersService;
+
+    @InjectMocks
+    TrainerController trainerController;
+
+    @Before
+    public void init(){
+        given(trainersService.getAllTrainers()).willReturn(prepareMockData());
+    }
+
 
     @Test
     void mainPage() {
+        System.out.println();
+    }
+
+   /* @Test
+    void showAllTrainers() {
+
+        List<Trainers> trainers = trainerController.showAllTrainers();
+
+        //then
+        Assert.assertThat(trainers, Matchers.hasSize(2));
+
+
+    }*/
+
+    private List<Trainers> prepareMockData(){
+        List<Trainers> trainers = new ArrayList<>();
+            trainers.add(new Trainers("lukas", "wieczorkiewicz", "lwiecz@wp.pl", 22223));
+            trainers.add(new Trainers("dorota", "lukas", "lwiecz@de.pl", 11111));
+            return trainers;
     }
 }
